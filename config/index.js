c68b29bb0b5f1bf1d9d683758eb60ecbb9b8d037
Panupat-Chanate <panupat.c@psru.ac.th>
2021-02-08 13:22:27 +0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
 
-const DB_URI_PROD = "mongodb+srv://userosd:osd_SIP4321!@ichaterdb.ch3bn.mongodb.net/cookies?retryWrites=true&w=majority"
-
-// const DB_URI_PROD = "mongodb+srv://userosd:osd%401234@osd.aq4ge.mongodb.net/cookies_winper?retryWrites=true&w=majority"
+const DB_URI_PROD = ""
 module.exports = { DB_URI_PROD }
